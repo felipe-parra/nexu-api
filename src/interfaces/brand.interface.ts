86d008dd2e2cb3996d5a21cd9daa8c9f172045a7
Felipe Parra <fpr29.10@gmail.com>
2023-09-04 16:58:57 -0600

    Addin interfaces

---
+++ src/interfaces/brand.interface.ts
@@ -0,0 +1,9 @@
+export interface IBrand {
+  id?: number;
+  _id?: string;
+  name?: string;
+  nombre?: string;
+  average_price?: number;
+  createdAt?: string;
+  updatedAt?: string;
+}
---
+++ src/interfaces/model.interface.ts
@@ -0,0 +1,9 @@
+export interface IModel {
+  id?: number;
+  _id?: string;
+  name: string;
+  average_price: number;
+  brand: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
